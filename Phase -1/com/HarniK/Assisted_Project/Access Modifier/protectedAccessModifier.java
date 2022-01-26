396b1b9d40f9ae371f3903191bd60460ad255155
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-01-26 12:26:51 +0530

    Import package
    Import package for Protected Access Modifier

---
+++ Phase -1/com/HarniK/Assisted_Project/Access Modifier/protectedAccessModifier.java
@@ -0,0 +1,9 @@
+package com.HarniK.Assisted_Project1;
+
+public class protectedAccessModifier {
+	protected void display(){
+        System.out.println("It is protected access modifiers"); 
+    } 
+}
+
+
