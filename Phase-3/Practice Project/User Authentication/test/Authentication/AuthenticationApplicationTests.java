beb0cab33abc35bbbe019f4815f2e777bae82e14
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-02-12 16:01:27 +0530

    Add files via upload

---
+++ Phase-3/Practice Project/User Authentication/test/Authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.HarniK.Authentication;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
