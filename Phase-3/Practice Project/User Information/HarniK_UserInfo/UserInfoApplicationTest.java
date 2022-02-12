7925c819d7368a2d89687a7ff58b381eafd6996c
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-02-12 14:15:59 +0530

    Add files via upload

---
+++ Phase-3/Practice Project/User Information/HarniK_UserInfo/UserInfoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.HarniK_UserInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInfoApplicationTest{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
