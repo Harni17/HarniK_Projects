d9d52103426146484b48b82de4f4998f9e9519f1
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-02-13 18:48:25 +0530

    Add files via upload

---
+++ Phase-3/Assisted Project/Edge Server and Routing/SpringEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HarniK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
