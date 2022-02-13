f182803b76ab4ced11eaaee6d2f4f1bf7de545c1
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-02-13 17:52:17 +0530

    Add files via upload

---
+++ Phase-3/Assisted Project/Default and Custom/HarniK/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.HarniK.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.HarniK.beans.CustomEventPublisher;
+
+
+@Controller
+public class MainController {
+
+
+        
+           @RequestMapping(value = "/customevent", method = RequestMethod.GET)
+            public String customEvent(ModelMap map)
+            {
+                    String confFile = "main-servlet.xml";
+                ApplicationContext context = new ClassPathXmlApplicationContext(confFile);
+                CustomEventPublisher cvp =
+                        (CustomEventPublisher) context.getBean("customEventPublisher");
+                     
+                 cvp.publish();  
+                 cvp.publish();
+                return "customEvent";
+            }
+           
+}
