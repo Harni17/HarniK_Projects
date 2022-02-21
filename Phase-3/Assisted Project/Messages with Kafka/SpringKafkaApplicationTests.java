6e474c2e046bd4e1aa53d8a5d6aff69aa5e8a103
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-02-21 21:32:28 +0530

    Add files via upload

---
+++ Phase-3/Assisted Project/Messages with Kafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HarniK;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
