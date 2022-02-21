5ce3adc4c546be77bb28e453018d75c371c8cf44
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-02-21 22:12:16 +0530

    Add files via upload

---
+++ Phase-3/Assisted Project/Microservices Communication/RestApiOneApplication.java
@@ -0,0 +1,13 @@
+package com.HarniK.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiOneApplication.class, args);
+	}
+
+}
---
+++ Phase-3/Assisted Project/Microservices Communication/RestApiOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HarniK.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
